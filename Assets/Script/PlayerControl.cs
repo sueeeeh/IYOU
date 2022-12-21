fb0fddf267d245b343bc7f0f4a811444ee11b830
sueeeeh <olivila0501@gmail.com>
2022-12-21 16:07:51 +0900

    build wall, move ch
    벽 세우기,
    캐릭터 이동(캐릭터가 벽의 색으로 변함)

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -15,6 +15,15 @@ public class PlayerControl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal"); //가로, 오른쪽 버튼 : 0에서 점점 1로 증가, 왼쪽 버튼 : 0에서 -1로 점점 감소, 다 떼면 점점 0으로 감
         float v = Input.GetAxis("Vertical"); //세로
-        print(v);
+        Vector3 dir = new Vector3(h,v,0);
+        //transform.position += dir; //값을 강제로 주입 => 벽도 뚫음
+        GetComponent<Rigidbody2D>().velocity = dir*3;
     }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.collider.CompareTag("WALL")){
+            GetComponent<SpriteRenderer>().color = collision.collider.GetComponent<SpriteRenderer>().color;
+        }   
+    }
+
 }
